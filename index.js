8c77be150bf66fb6b85b70d2bbaa8249c40f6177
jedza-mi-je-otac <87701765+jedza-mi-je-otac@users.noreply.github.com>
2022-07-16 04:43:01 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const texts = ['75 godina Zemunskog kajakaštva'];
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+
+
+
+function type(){
+
+    if(count === texts.length){
+        count = 0;
+    }
+    currentText = texts[count];
+    letter = currentText.slice(0, ++index);
+
+    document.querySelector('.typing').textContent = letter;
+    if(letter.length === currentText.length)
+    {
+        count++;
+        index = 0;
+        return 0;
+    }
+    setTimeout(type, 300);
+}
+
+window.onload = function(){
+    type();
+}
